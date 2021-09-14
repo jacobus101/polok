a0f4d89744b80bb3f96fdb8c096b23ca1eedee19
root <root@pop-os.localdomain>
2021-09-13 20:37:42 -0500

    fix

--- pos_auto_invoice/controller/pos_controller.py
+++ pos_auto_invoice/controller/pos_controller.py
@@ -4,7 +4,6 @@ from odoo.addons.bus.controllers.main import BusController
 from odoo import api, http, SUPERUSER_ID
 from odoo.addons.web.controllers.main import ensure_db, Home, Session, WebClient
 from odoo.addons.point_of_sale.controllers.main import PosController
-from odoo.addons.base.ir.ir_qweb import AssetsBundle
 import json
 import logging
 import base64
