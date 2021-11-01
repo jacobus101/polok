c7245b59c44f473be210f13de88bf0536975bd3c
jacobus101 <jacobus101@gmail.com>
2021-11-01 17:44:50 -0500

    Delete pos_auto_invoice directory

--- pos_auto_invoice/model/pos_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-class pos_config(models.Model):
-    _inherit = "pos.config"
-
-    pos_auto_invoice = fields.Boolean('POS auto invoice',
-                                      help='POS auto to checked to invoice button',
-                                      default=1)
