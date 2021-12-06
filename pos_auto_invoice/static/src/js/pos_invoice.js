417f09ceb60c0793122a6869d45f549c6c037ac6
Santiago Lopez <jacobus101@gmail.com>
2021-12-05 20:16:59 -0500

    fix

--- pos_auto_invoice/static/src/js/pos_invoice.js
+++ pos_auto_invoice/static/src/js/pos_invoice.js
@@ -11,8 +11,8 @@ odoo.define('pos_auto_invoice.pos_auto_invoice', function (require) {
 			if(this.pos.config.invoice_type_setting === 'pos_invoice' && this.pos.config.module_account)
 			{
 		            var order = this.pos.get_order();
-                            order.set_to_invoice(!order.is_to_invoice(true));
                             if (order.is_to_invoice()) {
+                                order.set_to_invoice(true);
                                 this.$('.js_invoice').addClass('highlight');
                                 this.$('.js_electronic_invoice').removeClass('hidden');
                             }
@@ -20,8 +20,8 @@ odoo.define('pos_auto_invoice.pos_auto_invoice', function (require) {
                         if(this.pos.config.invoice_type_setting === 'electronic_invoice' && this.pos.config.module_account)
                         {
                             var order = this.pos.get_order();
-                            order.set_to_electronic_invoice(!order.is_to_electronic_invoice(true));
                             if (order.is_to_electronic_invoice()) {
+                                order.set_to_electronic_invoice(true);
                                 this.$('.js_electronic_invoice').addClass('highlight');
                                 this.$('.js_invoice').addClass('highlight');
                                 this.$('.js_electronic_invoice').removeClass('hidden');
