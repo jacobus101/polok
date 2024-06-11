09fa02e9d26e1f14ba4087652eecf761a2dc1831
jacobus101 <jacobus101@gmail.com>
2024-06-10 22:53:11 -0500

    Update pos_invoice.js

--- pos_auto_invoice/static/src/js/pos_invoice.js
+++ pos_auto_invoice/static/src/js/pos_invoice.js
@@ -5,27 +5,32 @@ odoo.define('pos_auto_invoice.pos_auto_invoice', function (require) {
     var screens = require('point_of_sale.screens');
 
     screens.PaymentScreenWidget.include({
-		show: function() {
-			var self = this;
-			this._super();
-			if(this.pos.config.invoice_type_setting === 'pos_invoice' && this.pos.config.module_account){
-		        var pos_order = this.pos.get_order();
-                    if (pos_order !== null) {
-                        pos_order.set_to_invoice(true);
-                        this.$('.js_invoice').addClass('highlight');
-                        this.$('.js_electronic_invoice').removeClass('hidden');
-                    }
-			}
-            if(this.pos.config.invoice_type_setting === 'electronic_invoice' && this.pos.config.module_account){
-                var pos_order = this.pos.get_order();
-                    if (pos_order !== null) {
-                        pos_order.set_to_electronic_invoice(true);
-                        this.$('.js_electronic_invoice').addClass('highlight');
-                        this.$('.js_invoice').addClass('highlight');
-                        this.$('.js_electronic_invoice').removeClass('hidden');
-                    }
+        show: function() {
+            var self = this;
+            this._super();
+            var pos_order = this.pos.get_order();
+
+            if (this.pos.config.invoice_type_setting === 'pos_invoice' && this.pos.config.module_account) {
+                if (pos_order !== null) {
+                    pos_order.set_to_invoice(true);
+                    this.$('.js_invoice').addClass('highlight');
+                    this.$('.js_electronic_invoice').removeClass('hidden');
+                }
             }
 
-		}
-	});
+            if (this.pos.config.invoice_type_setting === 'electronic_invoice' && this.pos.config.module_account) {
+                if (pos_order !== null) {
+                    pos_order.set_to_electronic_invoice(true);
+                    this.$('.js_electronic_invoice').addClass('highlight');
+                    this.$('.js_invoice').addClass('highlight');
+                    this.$('.js_electronic_invoice').removeClass('hidden');
+                }
+            }
+
+            if (this.pos.config.invoice_type_setting === 'hide_invoice') {
+                this.$('.js_invoice').addClass('hidden');
+                this.$('.js_electronic_invoice').addClass('hidden');
+            }
+        }
+    });
 });
