7f0bd2b8355b99a67aafdbc1cfb0f15fdcdd1306
jacobus101 <jacobus101@gmail.com>
2024-06-10 23:18:10 -0500

    Update __manifest__.py

--- pos_email_validation/__manifest__.py
+++ pos_email_validation/__manifest__.py
@@ -1,1 +1,21 @@
-
+{
+    'name': 'POS Email Validation',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'summary': 'Validate email format in POS client form',
+    'description': """
+        This module adds email format validation to the Point of Sale client form.
+    """,
+    'author': 'Santiago López',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_email_validation_templates.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_email_validation/static/src/js/pos_email_validation.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
