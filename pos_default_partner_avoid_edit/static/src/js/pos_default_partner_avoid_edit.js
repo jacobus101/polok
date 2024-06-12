30e4521445f87761a9c5a96344d7b5c1ca008991
jacobus101 <jacobus101@gmail.com>
2024-06-11 22:41:51 -0500

    Update pos_default_partner_avoid_edit.js

--- pos_default_partner_avoid_edit/static/src/js/pos_default_partner_avoid_edit.js
+++ pos_default_partner_avoid_edit/static/src/js/pos_default_partner_avoid_edit.js
@@ -6,7 +6,7 @@ odoo.define('pos_default_partner_avoid_edit.disable_edit_button', function(requi
     screens.ClientListScreenWidget.include({
         display_client_details: function(visibility, partner, clickpos) {
             this._super(visibility, partner, clickpos);
-            var editButton = this.$('.edit-buttons');
+            var editButton = this.$('.button.edit');
             var defaultPartnerId = this.pos.config.default_partner_id;
 
             if (partner && partner.id === defaultPartnerId) {
