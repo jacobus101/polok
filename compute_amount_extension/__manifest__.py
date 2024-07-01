2ee4186f6c6054de14c11e50071f3880b3be3a4d
jacobus101 <jacobus101@gmail.com>
2024-06-30 21:21:36 -0500

    Create __manifest__.py

---
+++ compute_amount_extension/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Compute Amount Extension',
+    'version': '12.0.1.0.0',
+    'summary': 'Module to expose a public method for compute amount',
+    'category': 'Tools',
+    'author': 'Santiago López',
+    'depends': ['base', 'account'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
