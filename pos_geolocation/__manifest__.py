f84745e873fa98f49424ad3bd19933b5be08f883
jacobus101 <jacobus101@gmail.com>
2025-01-10 21:18:44 -0500

    Create __manifest__.py

---
+++ pos_geolocation/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'POS Geolocation',
+    'version': '1.0',
+    'summary': 'Adds geolocation functionality to the POS',
+    'description': """
+        This module adds geolocation tracking to Point of Sale orders.
+    """,
+    'author': 'Santiago López',
+    'website': 'https://polok.co',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_order_views.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_geolocation/static/src/js/pos_geolocation.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
