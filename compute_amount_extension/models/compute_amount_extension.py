88d04210950f792d97ecac74964b12101d26077c
jacobus101 <jacobus101@gmail.com>
2025-02-01 19:38:17 -0500

    Update compute_amount_extension.py

--- compute_amount_extension/models/compute_amount_extension.py
+++ compute_amount_extension/models/compute_amount_extension.py
@@ -1,9 +1,8 @@
 from odoo import models, api
 
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice' 
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
-    @api.multi
     def call_compute_amount(self):
         for record in self:
             record._compute_amount()
