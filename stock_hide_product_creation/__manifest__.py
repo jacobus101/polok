da6ec84a4374d2b80db32fe60ae9c8961f29d922
jacobus101 <jacobus101@gmail.com>
2025-03-14 00:42:59 -0500

    Update __manifest__.py

--- stock_hide_product_creation/__manifest__.py
+++ stock_hide_product_creation/__manifest__.py
@@ -1,8 +1,15 @@
 {
-    'name': 'Ocultar creación/edición rápida de productos en Stock Picking',
+    'name': 'Hide Product Creation in Order Forms',
     'version': '16.0.1.0.0',
-    'category': 'Stock',
-    'summary': 'Oculta la creación rápida y la opción de crear/editar productos en los formularios de stock.picking',
+    'category': 'Sales/Purchase/Inventory',
+    'summary': 'Disables quick creation, creation, and editing of products in stock picking, purchase order, and sales order forms.',
+    'description': """
+This module disables the ability to quickly create, create, or edit products from the following forms:
+- Stock Picking: Hides these actions in the move lines.
+- Purchase Order: Removes the create and edit options in order lines.
+- Sales Order: Prevents both template and variant creation or editing actions in order lines.
+By enforcing these restrictions, users are limited to selecting only existing products.
+""",
     'author': 'Santiago Lopez',
     'license': 'AGPL-3',
     'depends': ['stock', 'purchase', 'sale_management'],
