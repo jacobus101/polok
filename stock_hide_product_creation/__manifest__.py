5d7a5e1030e5adecc0f6f4e6692747f9a7849b44
jacobus101 <jacobus101@gmail.com>
2025-03-13 21:04:09 -0500

    Create __manifest__.py

---
+++ stock_hide_product_creation/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Ocultar creación/edición rápida de productos en Stock Picking',
+    'version': '16.0.1.0.0',
+    'category': 'Stock',
+    'summary': 'Oculta la creación rápida y la opción de crear/editar productos en los formularios de stock.picking',
+    'author': 'Santiago Lopez',
+    'license': 'AGPL-3',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
