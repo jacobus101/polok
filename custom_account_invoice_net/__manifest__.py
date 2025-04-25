a57e73006da6a43698535c63ff6c4967bb63b6ff
Santiago Lopez <jacobus101@gmail.com>
2025-04-25 16:41:45 -0500

    feat: agregar módulo custom_account_invoice_net

---
+++ custom_account_invoice_net/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Invoice Net Subtotals',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': ['views/report_invoice_net.xml'],
+    'installable': True,
+}
